ffdc49bfb0e3d7322c69cdd678bd05bf9d8c6c61
Peyal Hasan <peyalhasan143@gmail.com>
2026-01-22 20:11:42 +0600

    Add resume

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,7 +1,7 @@
 
 import { Typewriter } from "react-simple-typewriter";
 import Tilt from 'react-parallax-tilt';
-import PeyalHasanResume from './assets/PeyalHasanResume.pdf'
+import PeyalHasanResume from './assets/Peyal Hasan Resume.pdf'
 
 const Hero = () => {
   return (
